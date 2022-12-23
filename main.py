d57388e04a0a1c1c0d9f876dbd0af673dc7e7a39
Vadim Salomatin <vadim.d.salomatin@gmail.com>
2022-12-23 11:36:11 +0300

    my second commit

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ import random
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QPushButton
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)
+        self.init_ui()
+
+
+    def init_ui(self):
+        self.setGeometry(500, 250, 1000, 1000)
+        self.setWindowTitle('Рисование')
+        self.pushButton = QPushButton('Рисовать', self)
+        self.pushButton.move(10, 10)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -31,9 +38,9 @@ class MyWidget(QMainWindow):
             qp = QPainter()
             # Начинаем процесс рисования
             qp.begin(self)
-            qp.setBrush(QColor(255, 204, 0))
+            qp.setBrush(QColor(random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
             # Рисуем прямоугольник заданной кистью
-            qp.drawEllipse(random.randrange(50, 1000), random.randrange(50, 800), 100, 100)
+            qp.drawEllipse(random.randrange(50, 850), random.randrange(50, 850), 100, 100)
             # Завершаем рисование
             qp.end()
         except Exception as e:
